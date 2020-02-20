00df155ddbdb9ad10b9647534906fa686bceda3b
RamyaJupaka <56325048+RamyaJupaka@users.noreply.github.com>
2020-02-20 16:13:57 -0500

    SimpleArray_Loops

---
+++ arrayproject2/arrayproject2/arrayproject2.cpp
@@ -0,0 +1,100 @@
+// arrayproject2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	/*int ar1[6];
+	for (int i = 0; i < 6; i + 1)
+	{
+		cout << "enter the number " << endl;
+		cin >> ar1[i];
+	}
+	for (int i = 0; i < 6; i++)
+	{
+		cout << ar1[i];
+
+	}
+*/
+// marks [90,55,80,72,91,69] calculate average
+
+	/*int sum = 0, max;
+
+	int marks[6] = { 90, 55, 80, 72, 91, 69 };
+	max = marks[0];
+
+	for (int i = 0; i < 6; i++)
+	{
+
+		sum = sum + marks[i];
+
+	}
+	for (int i = 1; i < 6; i++)
+	{
+
+		if (marks[i] > max)
+		{
+			max = marks[i];
+		}
+		cout << "max is :" << max << endl;
+	}
+
+	cout << "sum is  : " << sum << endl;
+	cout << "avaerage is : " << sum << endl;*/
+
+
+	// get 10 numbers save in array 
+
+	/*int numbers[10];
+	int even[10] = {};
+	
+	
+	for (int i = 0; i < 10; i++)
+	{
+	
+		cout << "Enter the numbers : " << endl;
+		cin >> numbers[i];
+
+
+	}
+	for (int i = 9 ; i >= 0; i--)
+	{
+		cout << "the reverse array : " << numbers[i] << endl;
+		
+	}
+	for ( int i = 0; i <  10; i++)
+	{
+		if (numbers[i] % 2 == 0)
+		{
+			
+			
+			cout << "even numbers are: " << numbers[i] << endl;
+
+			
+		}
+	}*/
+	
+
+
+
+
+	
+
+	system("pause");
+	system("cls");
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
